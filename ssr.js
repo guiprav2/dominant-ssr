8567ec89bffd53a3f067ed2f7b210969b7a6c1b8
Camila Prá <camila@guiprav.com>
2025-12-03 06:08:21 -0300

    .

--- ssr.js
+++ ssr.js
@@ -1,13 +1,11 @@
 import server from './server.js';
+import { Window } from './happy-dom/lib/index.js';
+globalThis.window = new Window();
+for (let x of ['MutationObserver', 'Node', 'document']) globalThis[x] = window[x];
+let d = (await import('./dominant.js')).default;
 
 server.get('/', async req => {
   let interval = setInterval(async () => { i++; await d.updateSync() });
-  req.on('close', () => clearInterval(interval);
-  return d.html`
-    <html>
-      <body>
-        ${d.text(() => `Counter: ${i}`)}
-      </body>
-    </html>
-  `;
+  req.on('close', () => clearInterval(interval));
+  return d.el('html', d.el('body', d.el('div', 'Hello')));
 });
