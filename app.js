9333aad0ab768535951256295e73dcac09ced0d6
Camila Prá <camila@guiprav.com>
2025-12-03 08:44:55 -0300

    .

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ ews(app);
 ssr(app);
 app.ssr('/', async (ds, win, req) => {
   let i = 0;
+  let name = '';
+  let typedName;
   win.setInterval(() => { i++; ds.update() }, 1000);
-  win.document.body.append(ds.text(() => `Counter: ${i}`));
+  win.document.body.append(ds.el('div', [
+    ds.el('div', ds.text(() => `Counter: ${i}`)),
+    ds.el('div', [
+      ds.el('input', {
+        placeholder: `Enter your name...`,
+        value: ds.binding({ get: () => name, set: x => name = x }),
+      }),
+      ds.el('button', {
+        onclick: () => { typedName = name; ds.update() },
+        textContent: `Hello`,
+      }),
+    ]),
+    ds.if(() => typedName, ds.el('div', ds.text(() => `Hello, ${typedName}!`))),
+  ]));
 });
 let port = process.env.port || 3999;
 app.listen(port, () => console.log(`Listening:`, port));
--- dominant-ssr.js
+++ dominant-ssr.js
@@ -8,6 +8,26 @@ export default app => {
     let entry = live[id];
     if (!entry) { ws.close(); return }
     entry.ws = ws;
+    ws.on('message', data => {
+      data = JSON.parse(data);
+      switch (data.type) {
+        case 'click': {
+          let target = entry.win.document.querySelector(`[data-ssrid="${data.id}"]`);
+          target?.click?.();
+          break;
+        }
+        case 'input': {
+          let target = entry.win.document.querySelector(`[data-ssrid="${data.id}"]`);
+          if (!target) { console.warn(`Target not found (for ${data.id}):`, data.value); break }
+          target.setAttribute('value', target.value = data.value);
+          if (!target.bindings) break;
+          let b = target.bindings.find(x => x.key === 'value');
+          if (!b) break;
+          b.set(data.value);
+          break;
+        }
+      }
+    });
     ws.on('close', () => {
       entry.win.happyDOM.abort();
       entry.win.close();
@@ -26,6 +46,12 @@ export default app => {
       }, 10000);
       await fn(ds, win, req);
       await ds.updateSync();
+      function identify() {
+        for (let x of win.document.querySelectorAll('input, textarea, button')) {
+          if (x.getAttribute('data-ssrid')) continue;
+          x.setAttribute('data-ssrid', crypto.randomUUID());
+        }
+      }
       win.document.head.append(ds.el('script', { type: 'module' }, `
         import morphdom from 'https://esm.sh/morphdom';
         let id = ${JSON.stringify(id)};
@@ -35,9 +61,23 @@ export default app => {
           if (data.type !== 'diff') return;
           morphdom(document.documentElement, data.html);
         });
+        addEventListener('click', ev => {
+          let target = ev.target.closest('[data-ssrid]');
+          if (!target) return;
+          ws.send(JSON.stringify({ type: 'click', id: target.getAttribute('data-ssrid') }));
+        });
+        addEventListener('input', ev => {
+          let target = ev.target.closest('[data-ssrid]');
+          if (!target) return;
+          ws.send(JSON.stringify({ type: 'input', id: target.getAttribute('data-ssrid'), value: target.value }));
+        });
       `));
       res.end(win.document.documentElement.outerHTML);
-      entry.obs = new win.MutationObserver(() => entry.ws.send(JSON.stringify({ type: 'diff', html: win.document.documentElement.outerHTML })));
+      entry.obs = new win.MutationObserver(() => {
+        identify();
+        //entry.win.document.querySelectorAll('input, textarea').forEach(x => x.setAttribute('value', x.value));
+        entry.ws.send(JSON.stringify({ type: 'diff', html: win.document.documentElement.outerHTML }));
+      });
       entry.obs.observe(win.document.documentElement, { subtree: true, childList: true, attributes: true, characterData: true });
     } catch (err) {
       console.error(err);
